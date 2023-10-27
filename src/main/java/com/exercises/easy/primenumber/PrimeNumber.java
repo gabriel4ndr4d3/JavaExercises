214ef4e3e4eb46c87d97a67b46eadc0b289aa520
gabriel <mmmgabrieldeandrade@gmail.com>
2023-10-27 10:00:01 -0300

    Prime Numbers

--- src/main/java/com/exercises/easy/primenumber/PrimeNumber.java
+++ src/main/java/com/exercises/easy/primenumber/PrimeNumber.java
@@ -1,5 +1,7 @@
 package com.exercises.easy.primenumber;
 
+import java.util.Scanner;
+
 /**
  * Escreva um programa que receba um número inteiro e imprima na ultima linha "É primo" se o número for primo e "Não é primo" se não for primo.
  * <p>
@@ -8,6 +10,29 @@ package com.exercises.easy.primenumber;
  */
 public class PrimeNumber {
     public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        int numero = tec.nextInt();
+
+        if (numero == 1 || numero == 0){
+            System.out.println("Não é primo");
+            return;
+        }
+
+        boolean isPrimo = true;
+
+        for (int c = 2; c<numero; c++){
+           if (numero % c == 0){
+               isPrimo = false;
+               break;
+           }
+        }
+
+        if (isPrimo) {
+            System.out.println("É primo");
+        } else {
+            System.out.println("Não é primo");
+        }
 
     }
 }
