734635b361544d6b79bacf86e99120292d976f6b
Gabriel <mmmgabrieldeandrade@gmail.com>
2024-01-31 12:24:38 -0300

    Implementado opção "adicionar contato"

---
+++ src/main/java/com/exercises/easy/schedule/Contato.java
@@ -0,0 +1,14 @@
+package com.exercises.easy.schedule;
+
+public class Contato {
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Contato(String name, String email, String telefone) {
+        this.nome = name;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
--- src/main/java/com/exercises/easy/schedule/Schedule.java
+++ src/main/java/com/exercises/easy/schedule/Schedule.java
@@ -1,5 +1,9 @@
 package com.exercises.easy.schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Crie um programa que simule uma agenda de contatos. <br>
  * O programa deve ter as seguintes entradas;
@@ -17,7 +21,54 @@ package com.exercises.easy.schedule;
  * 2. Veja a lista de erros a serem emitidos na classe Error desse pacote. <br>
  */
 public class Schedule {
+
+   private List<Contato> contatos = new ArrayList<>();
+
     public static void main(String[] args) {
-        
+
+       Schedule schedule =  new Schedule();
+
+       schedule.run();
+
+    }
+
+    private void run() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("1 - Adicionar contato");
+        System.out.println("2 - Remover contato");
+        System.out.println("3 - Listar contatos");
+        System.out.println("0 - Sair");
+
+        int opcao;
+
+        do {
+            opcao = teclado.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    addContact();
+                    break;
+            }
+
+        } while (opcao != 0);
+    }
+
+    private void addContact() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        String name = teclado.nextLine();
+
+        System.out.print("Email: ");
+        String email = teclado.nextLine();
+
+        System.out.print("Telefone: ");
+        String telefone = teclado.nextLine();
+
+        System.out.println("");
+
+        contatos.add(new Contato(name,email,telefone));
     }
 }
+
