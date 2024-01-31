28b35a926f99c680cb647d7e1aec5c48535760cd
Irineu333 <mmmirineusilva@gmail.com>
2024-01-31 00:10:52 -0300

    doc(schedule): fix exercise's doc

--- src/main/java/com/exercises/easy/schedule/Schedule.java
+++ src/main/java/com/exercises/easy/schedule/Schedule.java
@@ -7,12 +7,14 @@ package com.exercises.easy.schedule;
  *     <li>1 - Adicionar contato</li>
  *     O programa deve registrar e guardar o nome, email e telefone do contato. Nessa ordem.
  *     <li>2 - Remove contato:</li>
- *     O programa deve registrar e guardar o nome, email e telefone do contato. Nessa ordem.
+ *     O programa  deve remover um contato pelo seu índice.
  *     <li>3 - Listar contatos:</li>
  *     O programa deve listar todos os contatos registrados, no seguinte formato: [indice] {nome} - [email] - {telefone}.
  *     <li>0 - Sair</li>
  *     O programa deve encerrar a execução.
  * </ul>
+ * 1. Apenas os resultados dos comandos 3 serão considerados. <br>
+ * 2. Veja a lista de erros a serem emitidos na classe Error desse pacote. <br>
  */
 public class Schedule {
 
