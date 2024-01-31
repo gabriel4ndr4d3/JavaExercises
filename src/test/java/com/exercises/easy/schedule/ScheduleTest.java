138bcc549295afa01d196409c267a36d6b829a48
Irineu333 <mmmirineusilva@gmail.com>
2024-01-30 23:55:49 -0300

    test(schedule): create "invalid index error test"

--- src/test/java/com/exercises/easy/schedule/ScheduleTest.java
+++ src/test/java/com/exercises/easy/schedule/ScheduleTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ScheduleTest extends IOTest {
 
@@ -52,6 +54,73 @@ class ScheduleTest extends IOTest {
                         "[1] Jane Doe - janedoe@email.com - 987654321"
                 ),
                 outputs
+        );
+    }
+
+    @Test
+    public void removeContactsTest() {
+
+        // given
+
+        setInput(
+                "1",
+                "Ana",
+                "ana123@email.com",
+                "123456789",
+                "1",
+                "Maria",
+                "maria123@email.com",
+                "987654321",
+                "2",
+                "0"
+        );
+
+        // when
+
+        Schedule.main(null);
+
+        // then
+
+        List<String> outputs = new ArrayList<>();
+
+        for (String output : getOutputs()) {
+            if (CONTACT_PATTERN.matcher(output).matches()) {
+                outputs.add(output);
+            }
+        }
+
+        assertEquals(
+                Arrays.asList(
+                        "[0] Maria - maria123@email.com - 987654321"
+                ),
+                outputs
+        );
+    }
+
+    @Test
+    public void invalidIndexErrorTest() {
+
+        // given
+
+        setInput(
+                "2",
+                "0",
+                "0"
+        );
+
+        // when
+
+        Schedule.main(null);
+
+        // then
+
+        Predicate<String> invalidIndexError =
+                output -> output.endsWith(Error.INVALID_INDEX.getMessage());
+
+        assertTrue(
+                getOutputs()
+                        .stream()
+                        .anyMatch(invalidIndexError)
         );
     }
 }
