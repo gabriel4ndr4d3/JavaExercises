cbb634322b6797f58438e0981db095d74e8b1f79
Irineu333 <mmmirineusilva@gmail.com>
2024-01-30 23:41:21 -0300

    doc(schedule): create exercise's doc

---
+++ src/main/java/com/exercises/easy/schedule/Schedule.java
@@ -0,0 +1,19 @@
+package com.exercises.easy.schedule;
+
+/**
+ * Crie um programa que simule uma agenda de contatos. <br>
+ * O programa deve ter as seguintes entradas;
+ * <ul>
+ *     <li>1 - Adicionar contato</li>
+ *     O programa deve registrar e guardar o nome, email e telefone do contato. Nessa ordem.
+ *     <li>2 - Remove contato:</li>
+ *     O programa deve registrar e guardar o nome, email e telefone do contato. Nessa ordem.
+ *     <li>3 - Listar contatos:</li>
+ *     O programa deve listar todos os contatos registrados, no seguinte formato: [indice] {nome} - [email] - {telefone}.
+ *     <li>0 - Sair</li>
+ *     O programa deve encerrar a execução.
+ * </ul>
+ */
+public class Schedule {
+
+}
